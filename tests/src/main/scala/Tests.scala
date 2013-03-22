218f84576b7241993554a17bd9322ebfa4556668
alwayslz <alwaysven@gmail.com>
2013-03-21 21:44:30 -0500

    add hello-world template

---
+++ tests/src/main/scala/Tests.scala
@@ -0,0 +1,20 @@
+package luc.edu.avians.tests
+
+import luc.edu.avians._
+import junit.framework.Assert._
+import _root_.android.test.AndroidTestCase
+import _root_.android.test.ActivityInstrumentationTestCase2
+
+class AndroidTests extends AndroidTestCase {
+  def testPackageIsCorrect() {
+    assertEquals("luc.edu.avians", getContext.getPackageName)
+  }
+}
+
+class ActivityTests extends ActivityInstrumentationTestCase2(classOf[MainActivity]) {
+   def testHelloWorldIsShown() {
+      val activity = getActivity
+      val textview = activity.findView(TR.textview)
+      assertEquals(textview.getText, "hello, world!")
+    }
+}
